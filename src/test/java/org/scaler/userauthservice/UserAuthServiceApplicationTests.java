e21d0578cda4d278be6810b5293f9fda8e2c8ff4
codingwithraghu <codingwithraghu@gmail.com>
2024-06-29 18:02:33 +0530

    First Commit

---
+++ src/test/java/org/scaler/userauthservice/UserAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.scaler.userauthservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserAuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
